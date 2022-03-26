db261c966b1312c667b8f101a29c2e0f2abaa1c2
merwanjdly <79838415+merwanjdly@users.noreply.github.com>
2022-03-26 12:37:48 +0100

    aaa
    aaaa
    
    aaaa
    
    aa

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      width: ['group-hover'],
+      height: ['group-hover'],
+    },
+  },
+  plugins: [],
+}
